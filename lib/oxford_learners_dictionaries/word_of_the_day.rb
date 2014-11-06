be4ad6ab06fc6cfa0a6084ea2baa26c0ff4e717c
Felipe Pelizaro Gentil <cdigentil@gmail.com>
2014-11-06 14:25:55 -0200

    Parse word of the day with short description using homepage

--- lib/oxford_learners_dictionaries/word_of_the_day.rb
+++ lib/oxford_learners_dictionaries/word_of_the_day.rb
@@ -1,33 +1,29 @@
 require 'nokogiri'
 require 'open-uri'
 
-module Oxford
-  module Learners
-    module Dictionaries
-      class WordOfTheDay
-        attr_reader :word, :short_definition#, :english
+module OxfordLearnersDictionaries
+  class WordOfTheDay
+    attr_reader :word, :short_definition#, :english
 
-        URL = "http://www.oxfordlearnersdictionaries.com"
+    URL = "http://www.oxfordlearnersdictionaries.com"
 
-        def initialize
-          parse
-        end
+    def initialize
+      parse
+    end
 
-        private
-        def parse
-          @wotd = Nokogiri::HTML(open(URL)).at("#daybox")
-          @word = word
-          @short_definition = short_definition
-        end
+    private
+    def parse
+      @wotd = Nokogiri::HTML(open(URL)).at("#daybox")
+      @word = parse_word
+      @short_definition = parse_short_definition
+    end
 
-        def word
-          wotd.css(".h").text unless wotd.nil?
-        end
+    def parse_word
+      @wotd.css(".h").text unless @wotd.nil?
+    end
 
-        def short_definition
-          wotd.css(".d").text unless wotd.nil?
-        end
-      end
+    def parse_short_definition
+      @wotd.css(".d").text unless @wotd.nil?
     end
   end
 end
