582d3a3e45e236c356ae33a2a791ea46a86eb750
Felipe Gentil <cdigentil@gmail.com>
2015-05-06 10:50:48 -0300

    Require new modules Definition/Example/Type

--- lib/oxford_learners_dictionaries.rb
+++ lib/oxford_learners_dictionaries.rb
@@ -1,5 +1,8 @@
 require 'oxford_learners_dictionaries/english'
 require 'oxford_learners_dictionaries/word_of_the_day'
+require 'oxford_learners_dictionaries/definition'
+require 'oxford_learners_dictionaries/example'
+require 'oxford_learners_dictionaries/type'
 
 module OxfordLearnersDictionaries
 end
