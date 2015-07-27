9cfb71e1346f8ef3dd1db5240e23bc9817717b8b
Felipe Gentil <cdigentil@gmail.com>
2015-07-27 09:36:47 -0300

    Definition parses single and multiple and push examples

--- lib/oxford_learners_dictionaries/definition.rb
+++ lib/oxford_learners_dictionaries/definition.rb
@@ -6,17 +6,21 @@ module OxfordLearnersDictionaries
 
     def initialize page
       @page = page
-      @signification = Array.new
+      @examples = Array.new
     end
 
     def parse_single_definition
       signification =  @page.css('.def')
       @signification = signification.count > 0 ? signification[0].text : signification.text
+      @examples.push(::OxfordLearnersDictionaries::Example.new(@page.css('.x-g')))
       self
     end
 
     def parse_multiple_definitions index
       @signification = @page.css('.def')[index].text
+      @page.css('.x-gs')[index].css('.x-g').each do |example|
+        @examples.push(::OxfordLearnersDictionaries::Example.new(example))
+      end
       self
     end
   end
